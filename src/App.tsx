7903db8fd7107941b1fa079cc157dede645b10ae
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-04-03 15:31:07 +0600

    Install and use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="min-h-screen w-full flex justify-center flex-col items-center">
+      <h1 className="text-3xl">
+        I am in center of page
+      </h1>
+      <button onClick={() => setCount(count + 1)} className="p-2 mt-4 text-blue-500 font-semibold  rounded border border-blue-500">
+        Count {count}
+      </button>
     </div>
   )
 }
